aee21c6771a678161e15c7e4a35b022af21e8b42
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-04 18:06:09 -0400

    Added initial files

---
+++ src/components/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+
+
+export default function App() {
+
+    const [userData, setUserData] = useState()
+    const [followers, setFollowers] = useState()
+    
+        useEffect(() => {
+            effect
+            return () => {
+                cleanup
+            };
+        }, [input])
+    
+      componentDidMount() {
+        console.log('In CDM')
+        this.fetchUser();
+      }
+    
+    
+      const fetchUser = () => {
+        const urls = [
+          'https://api.github.com/users/nezlobnaya',
+          'https://api.github.com/users/nezlobnaya/followers'
+        ]
+        Promise.all(urls.map(url => fetch(url)
+            .then(res => res.json())
+            .catch(err => console.error('Request failed', err))
+        ))
+        .then(data => this.setState({
+          userData: data[0],
+          followers: data[1]
+        }))
+        
+      }
+    
+    
+        return (
+          <>    
+            <UserList>
+              <div className='App'>
+              <h1>GitHub UserCards</h1>
+              <UserCard key={userData.login} {...userData} />
+              <Followers followers={followers} />
+              </div>
+            </UserList>
+            
+           </>
+        )
+      }
+    
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <h1>GitHub UserCards</h1>
+        </div>
+    )
+}
+
+
+
+
+
